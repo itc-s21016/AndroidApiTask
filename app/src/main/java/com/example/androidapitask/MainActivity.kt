a6dbb634baf1c71c54f8ba3f5a8c97ea0d37f17e
s21016 <s21016@std.it-college.ac.jp>
2023-04-11 22:37:54 +0900

    initial commit

---
+++ app/src/main/java/com/example/androidapitask/MainActivity.kt
@@ -0,0 +1,79 @@
+package com.example.androidapitask
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.example.androidapitask.databinding.ActivityMainBinding
+import okhttp3.*
+import okio.IOException
+import org.json.JSONObject
+
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // ホットペッパーAPIのURL
+        val apiUrl = "https://webservice.recruit.co.jp/hotpepper/gourmet/v1/"
+
+        // APIキー
+        val apiKey = "318a1003a75ce344"
+
+        // 現在地の緯度経度
+        val latitude = 35.681236
+        val longitude = 139.767125
+
+        // キーワード検索する場合
+        val keyword = "ラーメン"
+
+        // リクエストURLの作成
+        val url = "$apiUrl?key=$apiKey&lat=$latitude&lng=$longitude&range=3&keyword=$keyword"
+
+        // リクエストの送信
+        val request = Request.Builder().url(url).build()
+        val client = OkHttpClient()
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                Log.e("API Error", e.toString())
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                val json = response.body?.string()
+
+                // JSONをパースして必要な情報を取得する
+                val restaurants = parseJson(json)
+
+                // UIスレッドで表示するために、runOnUiThreadを使用する
+                runOnUiThread {
+                    // 検索結果をリストビューに表示する処理を実装する
+                }
+            }
+        })
+    }
+    // JSONをパースして必要な情報を取得する
+    fun parseJson(json: String?): List<Restaurant> {
+        val restaurants = mutableListOf<Restaurant>()
+        val jsonObject = JSONObject(json)
+        val results = jsonObject.getJSONObject("results")
+        val restaurantArray = results.getJSONArray("shop")
+
+        for (i in 0 until restaurantArray.length()) {
+            val restaurantObject = restaurantArray.getJSONObject(i)
+            val name = restaurantObject.getString("name")
+            val address = restaurantObject.getString("address")
+            val photoUrl = restaurantObject.getString("photo")
+            val latitude = restaurantObject.getDouble("lat")
+            val longitude = restaurantObject.getDouble("lng")
+            val restaurant = Restaurant(name, address, photoUrl, latitude, longitude)
+            restaurants.add(restaurant)
+        }
+
+        return restaurants
+    }
+
+
+}
---
+++ app/src/main/java/com/example/androidapitask/Restaurant.kt
@@ -0,0 +1,9 @@
+package com.example.androidapitask
+
+data class Restaurant(
+    val name: String,
+    val address: String,
+    val photoUrl: String,
+    val latitude: Double,
+    val longitude: Double
+)
